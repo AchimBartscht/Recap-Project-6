9a62eaf7fd8c2776f8d2243dd46c9a213a548a17
Achim Bartscht <beemerhh@gmail.com>
2023-09-26 13:14:08 +0200

    finish task one

--- pages/create.js
+++ pages/create.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 import Form from "../components/Form.js";
 import { StyledLink } from "../components/StyledLink.js";
+import useSWR from SWR
 
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
@@ -11,7 +12,8 @@ const StyledBackLink = styled(StyledLink)`
 export default function CreatePlacePage() {
   const router = useRouter();
 
-  function addPlace(place) {
+  export default function addPlace(place) {
+    const {mutate} = use
     console.log("Place added (but not really...)");
   }
 
